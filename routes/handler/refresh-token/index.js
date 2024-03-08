6774b8d1d5984ccb323c1e21101087e23d87ad01
wahyu <wahyuwidito24@gmail.com>
2024-03-08 13:48:44 +0700

    Service User

--- routes/handler/refresh-token/index.js
+++ routes/handler/refresh-token/index.js
@@ -1,6 +1,7 @@
 const create = require('./create');
-// const getToken = require('./getToken');
+const getToken = require('./getToken');
 
 module.exports = {
-  create
+  create,
+  getToken
 }
--- routes/refreshTokens.js
+++ routes/refreshTokens.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const refreshTokensHandler = require('./handler/refresh-tokens');
+const refreshTokensHandler = require('./handler/refresh-token');
 
 router.post('/', refreshTokensHandler.create);
-// router.get('/', refreshTokensHandler.getToken);
+router.get('/', refreshTokensHandler.getToken);
 
 module.exports = router;
